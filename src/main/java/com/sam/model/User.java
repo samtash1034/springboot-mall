21de660e395ec2e0b24accfe16b26e7bdd3c215a
sam <samtash1034@gmail.com>
2022-10-28 07:55:46 +0800

    10/28 帳號功能-單元測試

--- src/main/java/com/sam/model/User.java
+++ src/main/java/com/sam/model/User.java
@@ -11,12 +11,12 @@ public class User {
 
     private Integer userId;
 
-    @JsonProperty("e_mail")//返回給前端的json顯示不同名稱
+//    @JsonProperty("e_mail")//返回給前端的json顯示不同名稱
     private String email;
 
     @JsonIgnore//不會將password回傳給前端
     //user object to json 時，會忽略password變數
     private String password;
-    private Date createDate;
+    private Date createdDate;
     private Date lastModifiedDate;
 }
--- src/main/java/com/sam/rowmapper/UserRowMapper.java
+++ src/main/java/com/sam/rowmapper/UserRowMapper.java
@@ -15,7 +15,7 @@ public class UserRowMapper implements RowMapper<User> {
         user.setUserId(resultSet.getInt("user_id"));
         user.setEmail(resultSet.getString("email"));
         user.setPassword(resultSet.getString("password"));
-        user.setCreateDate(resultSet.getTimestamp("created_date"));
+        user.setCreatedDate(resultSet.getTimestamp("created_date"));
         user.setLastModifiedDate(resultSet.getTimestamp("last_modified_date"));
 
         return user;
